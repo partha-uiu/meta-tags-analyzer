6552c590fbb383417fc0a6900d59513dbab33044
Partha Sarathi Acharjya <39263124+partha-uiu@users.noreply.github.com>
2020-11-06 20:29:00 +0600

    plugin update

--- meta-tags-analyzer-shortcode.php
+++ meta-tags-analyzer-shortcode.php
@@ -1,25 +1,22 @@
 <?php 
 global $urlError;
 $url = isset($_POST['url']) ? $_POST['url']  : (isset($url) ? $url : NULL);
-
-$tags = get_meta_tags('http://www.cricinfo.com/');
-
 ?>
 <div>
     <form action="?" method="POST">
-        <div class="g-recaptcha" data-sitekey="6Ld57MMUAAAAAGe87D3ZQhr10-ITDSJ0CTKxTNE2"></div>
+        <div class="g-recaptcha" data-sitekey="<?php echo $recaptchaKey?>"></div>
         <br/>
         <input type="submit" value="Submit">
     </form>
 
     <form action="" method="post">
+    <?php echo (isset($urlError))? $urlError : NULL; ?>
         Enter a url: <input type="text" name="url" value="<?php echo $url; ?>">
+        
         <input type="submit" value="Submit" name="meta_tag_analyzer">
-        <?php  echo $urlError; ?>
+        
     </form>
 
     <h1>Hello</h1>
 </div>
 
-
-i
--- meta-tags-analyzer.php
+++ meta-tags-analyzer.php
@@ -7,6 +7,81 @@
 * Author: Partha
 * Author URI: 
 **/
+define( 'WP_DEBUG', true );
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+//add options page
+// Add user input page for name and value
+
+add_action('admin_menu', 'meta_tag_analyzer_options_page');
+
+
+function meta_tag_analyzer_options_page() {
+
+   $add_key_option_page = add_options_page('Add key', 'Key settings', 'manage_options', 'key-settings', 'key_input_page');
+   add_action( 'load-' . $add_key_option_page, 'load_admin_style' );
+}
+
+function load_admin_style() {
+    wp_register_style( 'custom_wp_admin_css', plugin_dir_url( __FILE__ ) . 'css/styles.css', false, '1.0.0' );
+    wp_enqueue_style( 'custom_wp_admin_css' );
+  }
+
+function key_input_page(){
+    global $recaptchaKeyError;
+    $getKey = get_option( 'recaptcha_key' );
+    $recaptchaKey = $getKey ? $getKey:NULL;
+
+    echo <<<HTML
+    <h3>Please enter the Data Key for recaptcha</h3>
+
+    <div class="container">
+        <form action="" method="post">
+            <label for="data-key">Data Key</label>
+            <input type="text" id="data-key" name="key" value='{$recaptchaKey}' placeholder="Data Key . . .">
+            {$recaptchaKeyError}
+            <input type="submit" value="Save">
+           
+        </form>
+    </div>
+HTML;
+}
+
+
+add_action('admin_init', 'captcha_key_validation');
+
+//End user input page
+function captcha_key_validation(){
+    global $recaptchaKeyError;
+   
+    if (isset($_POST['key'])) {
+        if(empty($_POST['key'])) {
+                $recaptchaKeyError = 'Recaptcha Key name must be filled';
+        }
+    }
+
+    if(isset($recaptchaKeyError)){
+        $recaptchaKeyError = "<span class=\"text-danger\">$recaptchaKeyError</span>";
+    }
+
+    if(isset($recaptchaKeyError)){
+        return;
+    }
+
+   $recaptchaKey = $_POST['key'];
+   $checkExistingKey = get_option('recaptcha_key');
+  
+
+   if($checkExistingKey) {
+    update_option( 'recaptcha_key', $recaptchaKey );
+   }else {
+    add_option( 'recaptcha_key', $recaptchaKey, '', 'yes' ); 
+   }
+}
 
 
 
@@ -24,9 +99,6 @@ function meta_tags_analyzer() {
 } 
 
 add_shortcode( 'meta_tags_analyzer', 'meta_tags_analyzer');
-
-
-
 
 
 function url_validation(){
@@ -53,11 +125,17 @@ function url_validation(){
     if(isset($urlError)){
         $urlError = "<span class=\"text-danger\">$urlError</span>";
         return;
+    } else{
+        $urlError ='';
     }
+
     $url = $_POST['url'];
     $tags = get_meta_tags($url);
     
     echo "<table>";
+    echo "<tr >";
+    echo "<th  style=\"text-align: center;\"  colspan=\"2\">Meta tags of:  <span class=\"text-muted\"> $url</span> </th>";
+    echo "</tr>";
     echo "<tr>";
     echo "<th>Meta</th>";
     echo "<th>Description</th>";
